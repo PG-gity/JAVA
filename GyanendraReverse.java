50b14fed92997c20aa1244d3c0556bb0fc258b63
PG-gity <102179778+PG-gity@users.noreply.github.com>
2022-04-19 19:20:03 +0530

    To reverse the letters of a Word/Name

---
+++ GyanendraReverse.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class GyanendraReverse {
+    public static void main(String[] args){
+        String name= "Gyanendra";
+        char []array=name.toCharArray();
+        for(int i = 8;i>-1;i--){
+            System.out.print(array[i]);
+
+        }
+
+
+    }
+}
